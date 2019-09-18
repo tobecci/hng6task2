28912a472d476cd64a4b85d9b690b8d167a368f9
adeyemionward <adeyemiadeshina6@gmail.com>
2019-09-18 08:54:03 +0100

    logout function, json to and from and registration created

---
+++ index.php
@@ -0,0 +1,5 @@
+<!-- <form action="utility.php" method="post">
+    <input class="form-control" type="text" name="name">
+     <input class="form-control" type="text" name="age">
+     <input class="form-control" type="submit" name="submit">
+</form> -->
