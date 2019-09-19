b29798b85e370ff1bf0ed28df05321c7c8fee8a5
adeyemionward <adeyemiadeshina6@gmail.com>
2019-09-19 12:52:03 +0100

    merge resolved

--- controllers/login_handler.php
+++ controllers/login_handler.php
@@ -7,26 +7,7 @@ $path = '../models/Model.php';
 require_once($path);
 
 //check the HTTP Request method 
-<<<<<<< HEAD
-if(filter_input(INPUT_SERVER, 'REQUEST_METHOD')=='POST'){
-    $password = filter_input(INPUT_POST, 'password') ;
-    $email = filter_input(INPUT_POST, 'email') ;
-    
-<<<<<<< HEAD
-    $isValid = $model::validate($email , $password); 
-=======
-    $isValid = Model::validate($email , $password) ; 
->>>>>>> 0e705535fa3eb7559029ca5209fe02c73ff8410e
-     if($isValid){//redirect control to the success page
-         session_destroy();
-         $success_path = '../pages/success.php' ;
-          header("Location: ".$success_path) ;
-     }
-     else{// redirect control back to login( index.php)
-         $index_path = '../index.php'  ;
-         header("Location: ".$index_path) ;
-     }
-=======
+
 if (filter_input(INPUT_SERVER, 'REQUEST_METHOD') == 'POST') {
     $password = filter_input(INPUT_POST, 'password');
     $email = filter_input(INPUT_POST, 'email');
@@ -40,5 +21,5 @@ if (filter_input(INPUT_SERVER, 'REQUEST_METHOD') == 'POST') {
         $index_path = '../index.php';
         header("Location: " . $index_path);
     }
->>>>>>> c81f6995805166e800fd2f1f1abe74c5aaf6e38f
+
 }
